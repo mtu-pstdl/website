c16a515a59e789e92829977efcd152779976dbc7
Elijah Cobb <elijah@elijahcobb.com>
2020-09-04 11:02:02 -0400

    remove rocket

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -29,7 +29,7 @@ export class HomePage extends React.Component<HomePageProps, HomePageState> {
 			<div className={"header"}>
 				<span className={"title"}>Planetary Surface Technology Development Lab</span>
 				<span className={"subtitle"}>Michigan Technological University</span>
-				<img src={"./rocket.svg"} className={"rocket"} alt={"rocket"}/>
+				{/*<img src={"./rocket.svg"} className={"rocket"} alt={"rocket"}/>*/}
 			</div>
 			<section style={{backgroundImage: "url(\"./render1.png\")"}}>
 				<h2>Lorem Ipsum Dolor</h2>
