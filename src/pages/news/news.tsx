056cfc0f84259cc329cb6d087ca0a2d6111bab60
Elijah Cobb <elijah@elijahcobb.com>
2020-11-30 11:20:21 -0500

    link

--- src/pages/news/news.tsx
+++ src/pages/news/news.tsx
@@ -36,6 +36,12 @@ const news: News[] = [
 		date: {month: 2, year: 2020, day: 14},
 		content: <p>MTU was awarded a grand to develop a small rover to lay lightweight, superconducting cable that tethers to a lander as it traverses craters in permanently shadowed regions. Once in its final destination, the rover acts as a recharging hub and communication relay for other robots working in the area, providing continuous power without requiring direct sunlight.</p>,
 		link: "https://www.nasa.gov/feature/langley/artemis-student-challenge-nasa-selects-university-teams-to-build-technologies-for-the-moon-s/"
+	},
+	{
+		title: "Breaking Rocks, Making Water: The Secret Formula for a Trip to Mars?",
+		date: {month: 1, day: 31, year: 2018},
+		content: <p>Michigan Tech researchers have been awarded a NASA grant to study ways to extract water from gypsum rock on Mars, bringing a human trip to the red planet closer to reality.</p>,
+		link: "https://www.mtu.edu/news/stories/2018/january/breaking-rocks-making-water-the-secret-formula-for-a-trip-to-mars.html"
 	}
 ];
 
